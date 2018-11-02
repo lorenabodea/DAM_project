5a189255da610ae5f42cda8ea6628a7b47392526
ioanabutoescu <ioanabutoescu@gmail.com>
2018-11-02 20:40:55 +0200

    Create project architecture

---
+++ app/src/main/java/ro/ase/proiect/dam_project/CreateContestActivity.java
@@ -0,0 +1,13 @@
+package ro.ase.proiect.dam_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateContestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_contest);
+    }
+}
---
+++ app/src/main/java/ro/ase/proiect/dam_project/HomeTeacherActivity.java
@@ -0,0 +1,13 @@
+package ro.ase.proiect.dam_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_teacher);
+    }
+}
