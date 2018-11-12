d1646c76c40d35f606e36ba6a7dc0fb6e31ed726
ioanabutoescu <ioanabutoescu@gmail.com>
2018-11-12 20:50:09 +0200

    Layout-uri pentru partea de profil student: create, edit, home, profile

--- app/src/main/java/ro/ase/proiect/dam_project/CreateProfileStudentActivity.java
+++ app/src/main/java/ro/ase/proiect/dam_project/CreateProfileStudentActivity.java
@@ -2,12 +2,22 @@ package ro.ase.proiect.dam_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CreateProfileStudentActivity extends AppCompatActivity {
+
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile_student);
+
+        initComponents();
+    }
+
+    public void initComponents()
+    {
+        btnSave = findViewById(R.id.create_profile_student_btn_save);
     }
 }
--- app/src/main/java/ro/ase/proiect/dam_project/MainActivity.java
+++ app/src/main/java/ro/ase/proiect/dam_project/MainActivity.java
@@ -1,13 +1,51 @@
 package ro.ase.proiect.dam_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnHomeStudent;
+    Button btnCreateProfileStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents()
+    {
+        btnHomeStudent = findViewById(R.id.main_btn_home_student);
+        btnHomeStudent.setOnClickListener(goToStudentHome());
+
+        btnCreateProfileStudent = findViewById(R.id.main_btn_create_profile_student);
+        btnCreateProfileStudent.setOnClickListener(goToCreateProfileStudent());
+    }
+
+    private View.OnClickListener goToStudentHome()
+    {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeStudentActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener goToCreateProfileStudent()
+    {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateProfileStudentActivity.class);
+                startActivity(intent);
+            }
+        };
     }
 }
